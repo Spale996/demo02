2fbcd14f6b192caf9cc7c2c1f86772f10b65707e
1289306868@qq.com <1289306868@qq.com>
2020-02-13 16:09:50 +0800

    项目初始化

---
+++ src/test/java/cn/zb/tinyf/TinyfApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zb.tinyf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
